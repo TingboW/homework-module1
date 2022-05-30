857e2bd607ad3b81c963ed3d8868183acd112aa4
TingboW <104877939+TingboW@users.noreply.github.com>
2022-05-30 01:43:31 -0700

    update submit button

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,15 +1,15 @@
 /*------- RANDOM CODES ------*/
-
+var code = ' '; // to store generated codes and initialize to empty value;
+var getCode = ' ';  //to store entered code
+var btnvalue;  // for button boolean value
+//create variable to hold the type of characters we want to show as codes
+var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
 //Function to generate combination of characters
 function generateCode(){
-    //Create variable to store generated codes and the type of characters we want to show as codes
-    var code = ''; //initialize to null value
-    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
-
     //Generate character multiple times using a loop
     for (i=1; i<=8;i++){
         var char = Math.random()* str.length; //random select a character from the variable and then store in a new variable
-        code += str.charAt(char) //accumulate the generated character into a string of 8 characters
+        code += str.charAt(char); //accumulate the generated character into a string of 8 characters
     }
     return code; //return the final accumulated string when loop ends
 }
@@ -18,8 +18,30 @@ function generateCode(){
 document.getElementById("codes").innerHTML = generateCode();
 
 //Disable Button
-function disableButton(){
-    document.getElementById("submit").disabled = true;
+function disableButton(btnvalue){
+    document.getElementById("submit").disabled = btnvalue;
+    if (btnvalue == true) {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
+}
+
+//listen to user input code
+var codebox = document.getElementById("codeentered"); //get textbox
+codebox.addEventListener("input", evaluateCode); //listen to code entered in textbox
+
+//run function is detected user had entered a character in textbox
+function evaluateCode() {
+    getCode = document.getElementById("codeentered").value; //get character entered
+    var charset1 = getCode.trim(); //remove any hidden characters entered
+    var charset2 = code.trim(); //remove any hidden characters generated
+//test if code entered matches the number of generated characters 
+    if (charset1.length == charset2.length && charset1 == charset2) {
+        disableButton(false); //if match, run the function to enable button
+    }
 }
 
 //Activate function
