6a5d6a764ddf2b2ae2d51d27514f490a91e424ff
TingboW <104877939+TingboW@users.noreply.github.com>
2022-06-05 18:35:27 -0700

    display cliff site names

--- js/activity.js
+++ js/activity.js
@@ -1,10 +1,35 @@
 $(document).ready(function(){
-    $("table tr").each(function(){
-        $(this).find("td:not(:first)").attr("id","selectable");
-        $(this).find("td:contains('Not')").attr("id","notSelectable");
-    })
-$("td[id='selectable']").click(function(){
-    $(this).toggleClass("highlight");
-})
+    $("td").click(function (){
+        var content = $(this).text();
 
-})
+        if (content != "Not Available"){
+            $(this).toggleClass("highlight");
+
+            if($(this).hasClass("highlight")) {
+                $('#displaySelected').css("visibility","visible");
+                $('#displaySelected').css("margin-top","2em");
+                var location;
+                if ($(this).index("td")%5 == 1){
+                    location = "West Cliff";
+                    console.log(location);
+                } else if ($(this).index("td")%5 == 2) {
+                    location = "North Cliff";
+                } else if ($(this).index("td")%5 == 3) {
+                    location = "East Cliff";
+                } else if ($(this).index("td")%5 == 4) {
+                    location = "South Cliff";
+                }
+
+                $('#result').append("<p>" + content + " at " + location + "</p>");
+
+            } else {
+                $('#result p:contains('+content+')').remove();
+            
+                if ($('#result').has('p').length == false){
+                    $('#displaySelected').css("visibility", "hidden");
+                    $('#displaySelected').css("margin-top","0");
+                }
+            }
+        }
+    });
+});
